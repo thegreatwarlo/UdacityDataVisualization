74a157cb376ddedef945aa0e66fa6e78ee35f8b7
thegreatwarlo <carloprovinciali@gmail.com>
2017-05-07 16:57:19 -0700

    Added on_time column on dataset

--- data_transform.py
+++ data_transform.py
@@ -9,7 +9,17 @@ import csv
 
 IN_FILE = "SAN_flight_history.csv"
 
+out_list = []
+
 with open(IN_FILE, 'rb') as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
-    for row in reader:
+    for in_row in reader:
+        out_row_arr = []
+        out_row_del = []
+        for i in range(5):
+            out_row_arr[i] = in_row[i]
+            out_row_arr[i] = in_row[i]
+        
+        
+        
