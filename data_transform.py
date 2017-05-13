ba302c881f73c915dd3ca6f53cf58f9f07bbe20b
thegreatwarlo <carloprovinciali@gmail.com>
2017-05-12 18:19:42 -0700

    Added categories for flight delays

--- data_transform.py
+++ data_transform.py
@@ -7,8 +7,8 @@ Created on Sat May 06 17:48:26 2017
 
 import csv
 
-IN_FILE = "SAN_flight_history.csv"
-OUT_FILE = "SAN_flight_history_parsed.csv"
+IN_FILE = "SAN_flight_history2.csv"
+OUT_FILE = "SAN_flight_history_parsed2.csv"
 
 def main():
     out_rows = []
@@ -17,23 +17,28 @@ def main():
         next(reader)
         
         for row_in in reader:
-            out_row_del = []
+            out_row_cd = []
+            out_row_od = []
             out_row_ot = []
-            for i in range(5):
-                out_row_del.append(row_in[i])
+            for i in range(4):
+                out_row_cd.append(row_in[i])
                 out_row_ot.append(row_in[i])
-            out_row_del.append('Delayed')
+                out_row_od.append(row_in[i])
+            out_row_cd.append('Carrier Delay')
+            out_row_od.append('Other Delay')
             out_row_ot.append('On Time')
             
-            out_row_del.append(row_in[8])
-            out_row_ot.append(row_in[9])
+            out_row_cd.append(row_in[8])
+            out_row_od.append(row_in[9])
+            out_row_ot.append(row_in[10])
+            out_rows.append(out_row_cd)
+            out_rows.append(out_row_od)
             out_rows.append(out_row_ot)
-            out_rows.append(out_row_del)
             
             
     with open(OUT_FILE, 'wb') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Year', 'Month', 'Date', 'Carrier', 'Carrier Name',
+        writer.writerow(['Year', 'Month', 'Carrier', 'Carrier Name',
                          'Type', 'Number of Flights'])
         for row in out_rows:
             writer.writerow(row)
